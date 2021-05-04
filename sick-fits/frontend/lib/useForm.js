16e079548a7a63d66eab91e9d337523d76586fba
Tdwyd1 <46451853+Tdwyd1@users.noreply.github.com>
2021-05-03 23:18:30 -0600

    changes

--- sick-fits/frontend/lib/useForm.js
+++ sick-fits/frontend/lib/useForm.js
@@ -1,4 +1,3 @@
-import { object } from 'prop-types';
 import { useState } from 'react';
 
 export default function useForm(initial = {}) {
@@ -7,14 +6,11 @@ export default function useForm(initial = {}) {
 
   function handleChange(e) {
     let { value, name, type } = e.target;
-    console.log(value);
     if (type === 'number') {
       value = parseInt(value);
     }
     if (type === 'file') {
-      // value = e.target.file;
-      console.log('here');
-      console.log(e.target);
+      [value] = e.target.files;
     }
     setInputs({
       ...inputs,
